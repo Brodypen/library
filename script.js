06eafdee0b27dee46bd0a6b871ac1713333bf3af
BrodyLinux <Frostwizard7987@gmail.com>
2022-06-02 16:43:13 -0500

    Add backbutton for modal

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const addBook = document.querySelector(".addBook");
 const readBook = document.querySelector(".readBook");
 const deleteBook = document.querySelector(".deleteBook");
 const submitBook = document.querySelector(".submitButton");
+const backButton = document.querySelector(".backButton");
 addBook.addEventListener("click", activeModal);
+backButton.addEventListener("click", unactiveModal);
 
 
 
@@ -14,8 +16,9 @@ addBook.addEventListener("click", activeModal);
 
 
 
-
-
+function unactiveModal(){
+    modal.classList.remove("modalActive");
+}
 
 function activeModal(){
     modal.classList.add("modalActive");
