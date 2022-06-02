fd48a9717dfea0881a19c882a34f0e187dedf6c3
BrodyLinux <Frostwizard7987@gmail.com>
2022-06-02 18:13:17 -0500

    Complete readBook button

--- script.js
+++ script.js
@@ -2,74 +2,93 @@
 const modal = document.querySelector(".modalContainer");
 /* buttons */
 const addBookButton = document.querySelector(".addBook");
-const readBook = document.querySelector(".readBook");
-const deleteBook = document.querySelector(".deleteBook");
 const bookForm = document.querySelector(".bookForm");
 const backButton = document.querySelector(".backButton");
 
 const bookGrid = document.querySelector(".bookGrid");
 let myLibrary = [];
 class Book {
-  constructor(title, author, page, finished) {
+  constructor(title, author, pages, finished) {
     this.title = title;
     this.author = author;
-    this.page = page;
+    this.pages = pages;
     this.finished = finished;
   }
 }
 
-
 const addBook = (e) => {
-    e.preventDefault();
-    const book = makeBook();
-    myLibrary.push(book);
-    unactiveModal();
-    updateBooks();
-}
-const displayBooks = (book)=>{
-      <div class="bookContainer">
-        <p class="bookTitle">Hey </p>
-        <p class="bookAuthor">Brody</p>
-        <p class="bookPages">10</p>
-        <button class="readBook">Finished</button>
-        <button class="deleteBook">Delete</button>
-      </div>;
-    const bookContainer = document.createElement('div');
-    const bookTitle = document.createElement('p');
-    const bookAuthor = document.createElement("p");
-    const bookPages = document.createElement("p");
-    const readBook = document.createElement("button");
-    const deleteBook = document.createElement("button");
-    
-    bookContainer.classList.add("bookContainer");
-    bookTitle.classList.add("bookTitle");
-    bookAuthor.classList.add("bookAuthor");
-    bookPages.classList.add("bookPages");
-    readBook.classList.add("readBook");
-    deleteBook.classList.add("deleteBook");
-}
+  e.preventDefault();
+  const book = makeBook();
+  myLibrary.push(book);
+  unactiveModal();
+  updateBooks();
+};
+const displayBooks = (book) => {
+  console.log(book);
+  const bookContainer = document.createElement("div");
+  const bookTitle = document.createElement("p");
+  const bookAuthor = document.createElement("p");
+  const bookPages = document.createElement("p");
+  const readBook = document.createElement("button");
+  const deleteBook = document.createElement("button");
+
+  bookContainer.classList.add("bookContainer");
+  bookTitle.classList.add("bookTitle");
+  bookAuthor.classList.add("bookAuthor");
+  bookPages.classList.add("bookPages");
+  readBook.classList.add("readBook");
+  deleteBook.classList.add("deleteBook");
+  /* TODO: ADD READBOOK AND DELETEBOOK BELOW*/
+  readBook.onclick = toggleFinished;
+  //   deleteBook.onclick = removeBook
+
+  bookTitle.textContent = `"${book.title}"`;
+  bookAuthor.textContent = `By ${book.author}`;
+  bookPages.textContent = `${book.pages} Pages`;
+  console.log(book.finished);
+  if (book.finished) {
+    readBook.textContent = "Finished";
+  } else {
+      readBook.classList.add("unfinished");
+    readBook.textContent = "Not Finished";
+  }
+  deleteBook.textContent = "Delete";
+
+  bookContainer.appendChild(bookTitle);
+  bookContainer.appendChild(bookAuthor);
+  bookContainer.appendChild(bookPages);
+  bookContainer.appendChild(readBook);
+  bookContainer.appendChild(deleteBook);
+  bookGrid.appendChild(bookContainer);
+};
 const updateBooks = () => {
-    bookGrid.innerHTML = '';
-    for(let book of myLibrary){
-        displayBooks(book);
-    }
-}
-
+  bookGrid.innerHTML = "";
+  for (let book of myLibrary) {
+    displayBooks(book);
+  }
+};
+const toggleFinished = (e) => {
+  const bookTitle = e.target.parentNode.firstChild.innerHTML.slice(1,-1);
+    const book = myLibrary.find(book => book.title === bookTitle);
+    book.finished = !book.finished;
+    console.log(book);
+  updateBooks();
+};
 function makeBook() {
-    const title = document.getElementById("title").value;
-    const author = document.getElementById("author").value;
-    const pages = document.getElementById("pages").value;
-    const isFinished = document.getElementById("isFinished").checked;
-    return new Book(title,author,pages,isFinished);
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const finished = document.getElementById("isFinished").checked;
+  return new Book(title, author, pages, finished);
 }
 
-function unactiveModal(){
-    modal.classList.remove("modalActive");
+function unactiveModal() {
+  modal.classList.remove("modalActive");
 }
 
-function activeModal(){
-    bookForm.reset();
-    modal.classList.add("modalActive");
+function activeModal() {
+  bookForm.reset();
+  modal.classList.add("modalActive");
 }
 addBookButton.addEventListener("click", activeModal);
 backButton.addEventListener("click", unactiveModal);
